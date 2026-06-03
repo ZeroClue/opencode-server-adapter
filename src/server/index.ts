02959b7aa768edd8cb3eb175b925ddd4a1f52e2f
ZeroClue <thezeroclue@gmail.com>
2026-06-03 07:39:04 +0200

    fix: add missing re-exports to server barrel

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,6 @@
 export { execute } from "./execute.js";
-export { ensureOpenCodeServerRunning, stopOpenCodeServer, getChildPid } from "./lifecycle.js";
+export { testEnvironment } from "./test.js";
+export { listOpenCodeServerModels } from "./models.js";
+export { getOpenCodeServerQuota } from "./stats.js";
 export { sessionCodec } from "./codec.js";
+export { ensureOpenCodeServerRunning, stopOpenCodeServer, getChildPid } from "./lifecycle.js";
